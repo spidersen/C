cb6df053270f52c1c83f1bd4d3de523ba396a193
spidersen <cristian.petersen@gmail.com>
2018-04-17 09:24:43 -0300

    Add files via upload

---
+++ Paralelos.c
@@ -0,0 +1,124 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define TAM 2
+
+int buscarLibre(int[], int);
+void mostrarAlumnos(int[], char[][20], int[], int[], float[], int);
+float calcularPromedio(int, int);
+int cargarAlumno(int[], char[][20], int[], int[], float[], int);
+
+
+
+int main()
+{
+    int legajo[TAM]= {};
+    char nombre[TAM][20];
+    int nota1[TAM];
+    int nota2[TAM];
+    float promedio[TAM];
+    int opcion;
+    int index;
+    int i;
+
+    do
+    {
+        printf("1. ALTAS\n2. MOSTRAR\n3. MODIFICAR\n4.BAJA\n9. SALIR\nElija una opcion: ");
+        scanf("%d", &opcion);
+
+        switch(opcion)
+        {
+        case 1:
+
+            index=cargarAlumno(legajo, nombre, nota1, nota2, promedio, TAM);
+            if(index == -1)
+            {
+                printf("No hay lugar");
+            }
+            else
+            {
+                printf("Alumno ingresado");
+            }
+            break;
+        case 2:
+
+        mostrarAlumnos(legajo, nombre, nota1, nota2, promedio, TAM);
+
+
+
+
+break;
+        }
+
+    }
+    while(opcion!=9);
+
+
+
+
+    return 0;
+}
+
+int buscarLibre(int legajos[], int tam)
+{
+    int index=-1;
+    int flag=0;
+    int i;
+    for(i=0; i<tam; i++)
+    {
+        if(legajos[i] == 0)
+        {
+            index = i;
+
+            break;
+        }
+    }
+
+
+    return index;
+}
+
+int cargarAlumno(int legajos[], char nombres[][20], int nota1[], int nota2[], float promedio[], int tam)
+{
+    int index ;
+    index = buscarLibre(legajos, tam);
+            if(index!=-1)
+            {
+                  printf("Ingrese legajo: ");
+            scanf("%d", &legajos[index]);
+
+            printf("Ingrese nombre: ");
+            fflush(stdin);
+            gets(nombres[index]);
+
+            printf("Ingrese Nota 1: ");
+            scanf("%d", &nota1[index]);
+
+            printf("Ingrese Nota 2: ");
+            scanf("%d", &nota2[index]);
+
+            promedio[index] = calcularPromedio(nota1[index], nota2[index]);
+            }
+            return index;
+}
+
+
+float calcularPromedio(int nota1, int nota2)
+{
+    float promedio;
+    promedio = (float)(nota1+nota2)/2;
+    return promedio;
+}
+
+void mostrarAlumnos(int legajos[], char nombres[][20], int nota1[], int nota2[], float promedio[], int tam)
+{
+    int i;
+    for(i=0; i<tam; i++)
+            {
+                if(legajos[i]!=0)
+                {
+                    printf("%d %s %d %d %f\n", legajos[i], nombres[i], nota1[i], nota2[i], promedio[i] );
+                }
+
+            }
+
+}
